39c45b23d0763f122fa05a57955604c59e58e803
paparao07 <kagit@Paparaokagitha>
2026-08-12 09:58:45 +0530

    commit

--- java.project/src/com/fundamentals/Student.java
+++ java.project/src/com/fundamentals/Student.java
@@ -2,21 +2,25 @@ package com.fundamentals;
 
 public class Student {
 	
-	int student_id;
-	String studentname;
-	int student_age;
+	 static int student_id;
+	static String studentname;
+	static int student_age;
 	void hello() {
 		System.out.println("hello Vcube");
 	}
 
+	
 	public static void main(String[] args) {
 		Student s1= new Student();
+		student_id = 07;
+		studentname = "paparao";
+		student_age =22;
 		s1.hello();
        System.out.println("main method started");
        
-       System.out.println(s1.student_id);
-       System.out.println(s1.studentname);
-       System.out.println(s1.student_age);
+       System.out.println("student_id:" + student_id);
+       System.out.println("studentname:" + studentname);
+       System.out.println("student_age :" +student_age);
        
        System.out.println("main method ended");
 	}
