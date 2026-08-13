4c7cfd9eea283ecb51b2520c645b7782e075ecf4
paparao07 <kagit@192.168.1.36>
2026-08-13 13:49:28 +0530

    commit

--- java.project/src/com/fundamentals/Student.java
+++ java.project/src/com/fundamentals/Student.java
@@ -5,17 +5,23 @@ public class Student {
 	 static int student_id;
 	static String studentname;
 	static int student_age;
-	void hello() {
+	
+	void $hello() {
 		System.out.println("hello Vcube");
 	}
-
+    void _$welcome() {
+    	System.out.println("Good Morning Students");
+    }
 	
 	public static void main(String[] args) {
-		Student s1= new Student();
+		
 		student_id = 07;
 		studentname = "paparao";
 		student_age =22;
-		s1.hello();
+		
+		Student s1= new Student();
+		s1.$hello();
+		s1._$welcome();
        System.out.println("main method started");
        
        System.out.println("student_id:" + student_id);
@@ -23,6 +29,25 @@ public class Student {
        System.out.println("student_age :" +student_age);
        
        System.out.println("main method ended");
+       
+       
+       student_id = 8;
+		studentname = "prasad";
+		student_age =22;
+		
+		Student s2= new Student();
+		s2.$hello();
+		s2._$welcome();
+		 System.out.println("main method started");
+	       
+	       System.out.println("student_id:" + student_id);
+	       System.out.println("studentname:" + studentname);
+	       System.out.println("student_age :" +student_age);
+	       
+	       System.out.println("main method ended");
+	       
+		
+
 	}
 
 }
